0a1a17382f2f0fcc17edf9a4b7a6c0fa2c159b7a
Whatislove <62023585+Shroud007@users.noreply.github.com>
2021-11-16 15:14:21 +0500

    feat(service): внесены доработки в сервис
    1. Методы py_transform, sim_transform и sim_check теперь приватные;
    2. Методы sim_check и check вместо кортежей возвращают словари (TypedDict);
    3. Исправлена ошибка, когда для задач на языке Java создавались файлы с неверным расширением (.cpp);
    4. Для методов класса CappaPlag добавлены cтроки документации.

--- src/app/service/main.py
+++ src/app/service/main.py
@@ -1,13 +1,20 @@
-from typing import TypedDict, List
-
 import pycode_similar
 import subprocess
 import re
+from typing import TypedDict, List
+
 
 class Candidate(TypedDict):
 
     uuid: str
     code: str
+
+
+class PlagResult(TypedDict):
+
+    uuid: str
+    result: int
+
 
 class Lang:
 
@@ -21,9 +28,15 @@ class Lang:
         (PYTHON, PYTHON)
     )
 
+
 class CappaPlag:
 
-    def py_transform(self, data) -> int:
+    def _py_transform(self, data) -> int:
+
+        """ Преобразует полученный в результате
+        применения Pycode_similar объект в строку, 
+        извлекает из нее вычисленный процент плагиата 
+        и возвращает его виде целого числа. """
 
         to_str = str(data)
         out = (re.findall("\d+\.\d+", to_str)).pop()
@@ -37,42 +50,63 @@ class CappaPlag:
             result = int(out[2:])
         return result
     
-    def sim_transform(self, data) -> int:
+    def _sim_transform(self, data) -> int:
+
+        """ Извлекает вычисленный процент плагиата из данных, 
+        полученных в результате применения детектора SIM, 
+        и возвращает его в виде целого числа. """
+
         out = (re.findall(r'\b\d+\b', data))[-1]
         result = int(out)
         return result
     
-    def sim_check(
+    def _sim_check(
         self, 
         sim_settings: str, 
+        sim_lang: Lang.CHOICES,
         ref_code: str, 
         candidate_info: List[Candidate]
-    ) -> tuple:
+    ) -> PlagResult:
         
+        """ Проверка на плагиат исходного кода задач на языках, 
+        поддерживаемых детектором SIM (C++, Java). """
+
         lenght = len(candidate_info)
         plag_dict = {}
-        ref = open("t1.cpp", "w+")
+        
+        if sim_lang == Lang.CPP:
+            file_1, file_2 = 't1.cpp', 't2.cpp'
+        elif sim_lang == Lang.JAVA:
+            file_1, file_2 = 't1.java', 't2.java'
+        
+        ref = open(file_1, "w+")
         ref.write(ref_code)
+        ref.close()
         for i in range(0, lenght):
             candidate_code = list(candidate_info[i].values())[1]
-            with open("t2.cpp", "w+") as cand:
+            with open(file_2, "w+") as cand:
                 cand.write(candidate_code)
-                ref.close()
                 cand.close()
             simcheck = subprocess.getoutput(sim_settings)
-            result = self.sim_transform(simcheck)
+            result = self._sim_transform(simcheck)
             plag_dict.update({list(candidate_info[i].values())[0]: result})
         plag_user_id = max(plag_dict, key=plag_dict.get)
         plag_score = max(plag_dict.values())
-        return plag_user_id, plag_score
+        plagiarism = PlagResult(
+            uuid = plag_user_id,
+            result = plag_score
+        )
+        return plagiarism
     
     def check(
         self, 
         lang: Lang.CHOICES, 
         ref_code: str, 
         candidate_info: List[Candidate]
-    ) -> tuple:
+    ) -> PlagResult:
         
+        """ Проверка исходного кода задач на наличие в нем плагиата. """
+
         if lang == Lang.PYTHON:
             lenght = len(candidate_info)
             plag_dict = {}
@@ -81,22 +115,30 @@ class CappaPlag:
                 pycheck = pycode_similar.detect([ref_code, candidate_code], 
                                           diff_method=pycode_similar.UnifiedDiff, 
                                           keep_prints=True, module_level=True)
-                result = self.py_transform(pycheck[0][1].pop(1))
+                result = self._py_transform(pycheck[0][1].pop(1))
                 plag_dict.update({list(candidate_info[i].values())[0]: result})
             plag_user_id = max(plag_dict, key=plag_dict.get)
             plag_score = max(plag_dict.values())
-            return plag_user_id, plag_score
+            plagiarism = PlagResult(
+                uuid = plag_user_id, 
+                result = plag_score
+            )
+            return plagiarism
         
         elif lang == Lang.CPP:
-            result = self.sim_check(
-                "sim_c++ -r4 -s -p  t1.cpp t2.cpp", 
-                ref_code, 
-                candidate_info)
+            result = self._sim_check(
+                sim_settings = "sim_c++ -r4 -s -p  t1.cpp t2.cpp", 
+                sim_lang = lang,
+                ref_code = ref_code, 
+                candidate_info = candidate_info
+            )
             return result
 
         elif lang == Lang.JAVA:
-            result = self.sim_check(
-                "sim_java -r4 -s -p  t1.java t2.java", 
-                ref_code, 
-                candidate_info)   
+            result = self._sim_check(
+                sim_settings = "sim_java -r4 -s -p  t1.java t2.java", 
+                sim_lang = lang,
+                ref_code = ref_code, 
+                candidate_info = candidate_info
+            ) 
             return result          
