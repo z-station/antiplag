8ed40ca15f5e6dc1ed994115e8fbeecce65675c0
azat.zakirov <zakirmalay@gmail.com>
2026-06-12 20:01:39 +0500

    fix(uvicorn): uvicorn заменен на gunicorn

--- src/start.sh
+++ src/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uvicorn app.main:app --host 0.0.0.0 --port 9020 --reload --workers ${UVICORN_WORKERS:=1}
+gunicorn app.main:app --bind 0.0.0.0:9020 --workers ${UVICORN_WORKERS:=1} --worker-class uvicorn.workers.UvicornWorker --log-level debug --timeout 60 --reload
